426878d4eb8fa670d1333ed4b38563857b9aeb85
mancorago-web <mancora.go@gmail.com>
2026-03-06 02:02:05 -0500

    force dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: ReactNode;
 }) {
   return (
-    <html lang="es">
+    <html lang="es" className="dark">
   <body>
     <RefreshRedirect />
     {children}
